4587e44b380522abbdcf024dd50f42d0759ca038
fewNeurons <ferranramirezmarti@gmail.com>
2021-02-05 18:05:54 +0100

    initial commit

---
+++ src/main/java/io/fewNeurons/springboot/PelisApp.java
@@ -0,0 +1,13 @@
+package io.fewNeurons.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelisApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PelisApp.class, args);
+	}
+
+}
